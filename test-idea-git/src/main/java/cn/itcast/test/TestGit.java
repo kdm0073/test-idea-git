10a65d7203ce1c29a72f8c7418f9e047d7546b3d
kdm <kongdeming@itcast.cn>
2018-12-19 19:05:52 +0800

    ff

---
+++ test-idea-git/src/main/java/cn/itcast/test/TestGit.java
@@ -0,0 +1,8 @@
+package cn.itcast.test;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("22222222");
+    }
+}
